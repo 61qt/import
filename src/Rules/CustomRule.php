64c5dde2411d7be912daf335ad88a6d90f5fb1b5
xurh <xurhwork@gmail.com>
2024-10-24 11:49:00 +0800

    ✨ feat: 添加自由校验规则

---
+++ src/Rules/CustomRule.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace QT\Import\Rules;
+
+use Throwable;
+use RuntimeException;
+use QT\Import\Contracts\Validatable;
+
+/**
+ * 自定义校验规则
+ *
+ * Class CustomRule
+ *
+ * eq:
+ * new CustomRule([$this, 'importDataValidator'])
+ * new CustomRule(function ($row, $line) {
+ *     if (empty($row['email'])) {
+ *         throw new RuntimeException('邮箱为空');
+ *     }
+ * })
+ *
+ * @package QT\Import\Rules
+ */
+class CustomRule implements Validatable
+{
+    /**
+     * 错误行
+     *
+     * @var array
+     */
+    protected $errors = [];
+
+    /**
+     * Undocumented function
+     */
+    public function __construct(protected $fn)
+    {
+        if (!is_callable($this->fn)) {
+            throw new RuntimeException('必须传入回调函数');
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param array $rows
+     * @param array $customAttributes
+     * @return boolean
+     */
+    public function validate($rows, $customAttributes = []): bool
+    {
+        foreach ($rows as $line => $row) {
+            try {
+                call_user_func($this->fn, $row, $line);
+            } catch (Throwable $e) {
+                if (empty($this->errors[$line])) {
+                    $this->errors[$line] = [];
+                }
+
+                $this->errors[$line][] = $e->getMessage();
+            }
+        }
+
+        return empty($this->errors);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return array
+     */
+    public function errors(): array
+    {
+        return $this->errors;
+    }
+}
